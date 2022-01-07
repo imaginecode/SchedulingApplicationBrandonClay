d8e6715532058863af929def61031cd707784b8b
Brandon Clay <imagesofclay@gmail.com>
2022-01-07 18:42:13 +0000

    Linking FXML to controllers

--- src/Controller/ModifyAppointment.java
+++ src/Controller/ModifyAppointment.java
@@ -1,10 +1,62 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 public class ModifyAppointment {
     public Button cancelUpdate;
     public Button saveUpdate;
     public TextField appointment_ID;
+    public TextField description;
+    public TextField location;
+    public TextField contact;
+    public TextField type;
+    public TextField Customer_ID;
+    public TextField User_ID;
+    public TextField title;
+    public DatePicker aptStartDate;
+    public ComboBox startTime;
+    public DatePicker aptEndDate;
+
+    public void cancelUpdateHandler(ActionEvent actionEvent) {
+    }
+
+    public void saveUpdateHandler(ActionEvent actionEvent) {
+    }
+
+    public void appointment_IDHandler(ActionEvent actionEvent) {
+    }
+
+    public void descriptionHandler(ActionEvent actionEvent) {
+    }
+
+    public void locationHandler(ActionEvent actionEvent) {
+    }
+
+    public void contactHandler(ActionEvent actionEvent) {
+    }
+
+    public void typeHandler(ActionEvent actionEvent) {
+    }
+
+    public void Customer_IDHandler(ActionEvent actionEvent) {
+    }
+
+    public void User_IDHandler(ActionEvent actionEvent) {
+    }
+
+    public void titleHandler(ActionEvent actionEvent) {
+    }
+
+    public void aptStartDateHandler(ActionEvent actionEvent) {
+    }
+
+    public void startTimeHandler(ActionEvent actionEvent) {
+    }
+
+    public void aptEndDateHandler(ActionEvent actionEvent) {
+    }
 }
