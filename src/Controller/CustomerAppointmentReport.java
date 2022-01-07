dc3405300e325c9d444eefb3b6a5ba993a8cf408
Brandon Clay <imagesofclay@gmail.com>
2022-01-07 18:42:36 +0000

    Linking FXML to controllers

---
+++ src/Controller/CustomerAppointmentReport.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class CustomerAppointmentReport {
+    public TableView appointmentTypes;
+    public TableColumn decCol;
+    public TableColumn novCol;
+    public TableColumn octCol;
+    public TableColumn sepCol;
+    public TableColumn augCol;
+    public TableColumn julCol;
+    public TableColumn juneCol;
+    public TableColumn mayCol;
+    public TableColumn marCol;
+    public TableColumn aprCol;
+    public TableColumn febCol;
+    public TableColumn janCol;
+    public TableView monthTable;
+    public Button exitReport;
+
+    public void exitReportHandler(ActionEvent actionEvent) {
+    }
+}
---
+++ src/Controller/CustomerScheduleReport.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+
+public class CustomerScheduleReport {
+    public ComboBox customerComboPick;
+    public Button exitReport;
+    public TableColumn customerID;
+    public TableColumn end;
+    public TableColumn start;
+    public TableColumn typeDescription;
+    public TableColumn title;
+    public TableColumn appointmentID;
+
+    public void customerComboPickHandler(ActionEvent actionEvent) {
+    }
+
+    public void exitReportHandler(ActionEvent actionEvent) {
+    }
+}
