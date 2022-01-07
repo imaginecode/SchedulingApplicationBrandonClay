a4aa638bc2a926f49fc9d7a264d0ed5d3f6ee13a
Brandon Clay <imagesofclay@gmail.com>
2022-01-07 18:46:57 +0000

    Linking FXML to controllers

--- src/Controller/Reports.java
+++ src/Controller/Reports.java
@@ -1,4 +1,30 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class Reports {
+    public Button genCustomerSchedule1;
+    public Button genCustomerSchedule;
+    public TextField searchPostalCode;
+    public Button exitReports;
+    public Button appointmentsByTimeMonth;
+    public ComboBox pickCustomerSchedule;
+
+    public void genCustomerScheduleHandler(ActionEvent actionEvent) {
+    }
+
+    public void pickCustomerScheduleHandler(ActionEvent actionEvent) {
+    }
+
+    public void appointmentsByTimeMonthHandler(ActionEvent actionEvent) {
+    }
+
+    public void ExitReportsHandler(ActionEvent actionEvent) {
+    }
+
+    public void searchPostalCodeHandler(ActionEvent actionEvent) {
+    }
 }
---
+++ src/Controller/ReportsByZip.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class ReportsByZip {
+    public TableView customersByZipTable;
+    public TableColumn Name;
+    public TableColumn Customer_IDPostalCode;
+    public Button exitReport;
+    public TextField searchPostalCode;
+
+    public void customersByZipTableHandler(SortEvent<TableView> tableViewSortEvent) {
+    }
+
+    public void exitReportHandler(ActionEvent actionEvent) {
+    }
+
+    public void searchPostalCodeHandler(ActionEvent actionEvent) {
+    }
+}
