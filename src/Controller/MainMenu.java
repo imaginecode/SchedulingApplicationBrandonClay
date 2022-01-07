5b050e22a70a2e4852e8eee010d1d57931d906f2
Brandon Clay <imagesofclay@gmail.com>
2022-01-07 23:37:17 +0000

    Linking FXML to controllers

--- src/Controller/MainMenu.java
+++ src/Controller/MainMenu.java
@@ -4,61 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 
 public class MainMenu {
+
     public Button makeAppointment;
     public Button Reports;
-    public Button addAppointment;
-    public Button exit;
-    public Button editCustomer;
-    public Button deleteApt;
-    public Button editAppointment;
-    public Button deleteAppointment;
-    public TableColumn address;
-    public TableView appointmentsTable;
-    public TableColumn description;
-    public TableColumn apt_ID;
-    public TableColumn location;
-    public TableColumn contact;
-    public TableColumn type;
-    public TableColumn startDate;
-    public TableColumn endDate;
-    public TableColumn customer_IDApts;
-    public TableColumn userID;
-    public RadioButton monthFilter;
-    public RadioButton weekFilter;
-    public TableView customerTable;
-    public TableColumn customer_ID;
-    public TableColumn name;
-    public TableColumn postal;
-    public TableColumn phone;
-    public ToggleGroup MonthWeek;
 
     public void reportsHandler(ActionEvent actionEvent) {
     }
 
     public void makeAppointmentHandler(ActionEvent actionEvent) {
-    }
-
-    public void deleteAppointmentHandler(ActionEvent actionEvent) {
-    }
-
-    public void editAppointmentHandler(ActionEvent actionEvent) {
-    }
-
-    public void deleteAptHandler(ActionEvent actionEvent) {
-    }
-
-    public void ExitHandler(ActionEvent actionEvent) {
-    }
-
-    public void addAppointmentHandler(ActionEvent actionEvent) {
-    }
-
-    public void editCustomerHandler(ActionEvent actionEvent) {
-    }
-
-    public void monthFilterHandler(ActionEvent actionEvent) {
-    }
-
-    public void weekFilterHandler(ActionEvent actionEvent) {
     }
 }
