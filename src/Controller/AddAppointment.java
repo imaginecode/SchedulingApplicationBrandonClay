0b5fb00523591327d8e70d76525ae1e2d6663eb0
Brandon Clay <imagesofclay@gmail.com>
2022-01-07 17:41:44 +0000

    Linking FXML to controllers

--- src/Controller/AddAppointment.java
+++ src/Controller/AddAppointment.java
@@ -1,4 +1,62 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class AddAppointment {
+    public ComboBox contactCombo;
+    public ComboBox endTime;
+    public ComboBox startTime;
+    public DatePicker aptDate;
+    public TextField title;
+    public TextField User_ID;
+    public TextField Customer_ID;
+    public TextField type;
+    public TextField aptLocation;
+    public TextField aptDescription;
+    public TextField appointment_ID;
+    public Button SaveAptAdd;
+    public Button cancelAptAdd;
+
+    public void cancelAptAddHandler(ActionEvent actionEvent) {
+    }
+
+    public void SaveAptAddHandler(ActionEvent actionEvent) {
+    }
+
+    public void appointment_IDHandler(ActionEvent actionEvent) {
+    }
+
+    public void aptDescriptionHandler(ActionEvent actionEvent) {
+    }
+
+    public void AptLocationHandler(ActionEvent actionEvent) {
+    }
+
+    public void handler(ActionEvent actionEvent) {
+    }
+
+    public void Customer_IDHandler(ActionEvent actionEvent) {
+    }
+
+    public void User_IDHandler(ActionEvent actionEvent) {
+    }
+
+    public void titleHandler(ActionEvent actionEvent) {
+    }
+
+    public void aptDateHandler(ActionEvent actionEvent) {
+    }
+
+    public void startTimeHandler(ActionEvent actionEvent) {
+    }
+
+    public void endTimeHandler(ActionEvent actionEvent) {
+    }
+
+    public void contactComboHandler(ActionEvent actionEvent) {
+    }
 }
--- src/Controller/ModifyAppointment.java
+++ src/Controller/ModifyAppointment.java
@@ -1,4 +1,10 @@
 package Controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class ModifyAppointment {
+    public Button cancelUpdate;
+    public Button saveUpdate;
+    public TextField appointment_ID;
 }
