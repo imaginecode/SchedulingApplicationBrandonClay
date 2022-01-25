3131f2307e57d74c14929ba8ec7b42576b77e04a
Brandon Clay <imagesofclay@gmail.com>
2022-01-25 15:28:44 -0600

    Starting to add getWeekAppointments

--- src/Data/AppointmentsData.java
+++ src/Data/AppointmentsData.java
@@ -1,6 +1,7 @@
 package Data;
 /**
- * This class contains MYSQL queries for appointments*/
+ * This class contains MYSQL queries for appointments
+ /** @author Brandon Clay */
 
 import Controller.MainMenu;
 import Model.Appointment;
@@ -10,6 +11,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.chart.ScatterChart;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,6 +23,8 @@ import java.time.LocalTime;
 
 public class AppointmentsData {
 
+    /** Query for all appointments.
+     * @return aptResultSet a result set of all appointments */
     public static ObservableList<Appointment> getAllAppointments() throws SQLException {
         ObservableList<Appointment> aptResultSet = FXCollections.observableArrayList();
         String query = "SELECT * FROM appointments";
@@ -61,6 +65,55 @@ public class AppointmentsData {
         return aptResultSet;
 
     }
+    /**Queries appointments for a one week period starting at current date and showing a week into the future
+     * @return aptResultSet result set from query */
+    public static ObservableList<Appointment> getWeekAppointments() throws SQLException {
+        ObservableList<Appointment> aptResultSet = FXCollections.observableArrayList();
+        LocalDate today = LocalDateTime.now().toLocalDate();
+        LocalDate weekTimeFrame = today.plusDays(7);
+        String query = "SELECT * FROM AppointmentsView WHERE Start < ? AND Start > ?";
+
+        DBQuery.setPreparedStatement(JDBC.getConnection(), query);
+        PreparedStatement preparedStatement = DBQuery.getPreparedStatement();
+
+        preparedStatement.setDate(1, Date.valueOf(today));
+        preparedStatement.setDate();
+
+        try {
+            preparedStatement.execute();
+            ResultSet rs = preparedStatement.getResultSet();
+            //Going through result set
+            while (rs.next()) {
+                int appointmentID = rs.getInt("Appointment_ID");
+                String appointmentTitle = rs.getString("Title");
+                String appointmentDescription = rs.getString("Description");
+                String appointmentLocation = rs.getString("Location");
+                String appointmentType = rs.getString("Type");
+
+                // times and dates converted to local time and date of user
+                LocalDateTime start = rs.getTimestamp("Start").toLocalDateTime();
+                LocalDateTime end = rs.getTimestamp("End").toLocalDateTime();
+
+                int customerID = rs.getInt("Customer_ID");
+                int userID = rs.getInt("User_ID");
+                int contactID = rs.getInt("Contact_ID");
+
+                System.out.println(appointmentTitle + appointmentDescription+appointmentLocation + appointmentType+start+end+
+                        " " +customerID + " " +userID+ " " +contactID);
+//                //Creating new appointment object
+                Appointment newApt = new Appointment(appointmentID, appointmentTitle, appointmentDescription, appointmentLocation,
+                        appointmentType, start, end , customerID, userID, contactID);
+                aptResultSet.add(newApt);
+
+            }
+        }
+        catch(SQLException e){
+            return null;
+        }
+        return aptResultSet;
+    }
+
+
 
 
 
