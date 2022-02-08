ef052e76ba2e8f9bd3423f9e471660613d24d1d5
Brandon Clay <imagesofclay@gmail.com>
2022-02-07 22:34:30 -0600

    Started appointment Checks to make sure appointment is valid

--- src/Controller/AddAppointment.java
+++ src/Controller/AddAppointment.java
@@ -7,10 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -45,6 +42,30 @@ public class AddAppointment implements Initializable {
     }
 
     public void SaveAptAddHandler(ActionEvent actionEvent) {
+        if (appointmentChecks()){
+
+        }
+        else{
+
+        }
+
+
+    }
+/**Checks to make sure appointment is valid fields are not empty as well as checking for overlapping appointments,
+ * valid dates, and that appointment is within business hours
+ * @return boolean of t/f for if the appointment is valid if (f) then alerts are displayed*/
+    private boolean appointmentChecks() {
+
+        //Checking to make sure text fields aren't empty
+
+        //Checking to make sure end date is after start date
+
+        //Checking for overlapping appointments
+
+        //Checks too see if appointment is with business hours defined as 8:00 a.m. to 10:00 p.m. EST, including weekends
+
+
+
     }
 
     public void appointment_IDHandler(ActionEvent actionEvent) {
@@ -87,6 +108,38 @@ public class AddAppointment implements Initializable {
 //    public void dataHandoff(Customer controller){
 //        Customer_ID.setText(String.valueOf(controller.getCustomerID()));
 //    }
+
+    /** Displays messages for add appointment errors such as empty fields
+     * @param alertnum is for selecting which specific alert case is needed */
+    public void addErrors(int alertnum) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+
+        switch (alertnum) {
+            case 1:
+                alert.setTitle("All fields must be filled");
+                alert.setHeaderText("All fields must be filled");
+                alert.setContentText("All fields  and combo boxes must be filled selected");
+                alert.showAndWait();
+                break;
+            case 2:
+                alert.setTitle("Confirm Save");
+                alert.setHeaderText("Confirm Save");
+                alert.setContentText("Confirm Save");
+                alert.showAndWait();
+                break;
+            case 3:
+                alert.setTitle("Confirm Cancel");
+                alert.setHeaderText("Confirm Cancel");
+                alert.setContentText("Confirm Cancel");
+                alert.showAndWait();
+                break;
+
+
+
+
+
+        }
+    }
 
 
 
